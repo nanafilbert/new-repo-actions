2ccb88acd209141c2fa0e234d6409a2ecf367b07
Nana Faty <filbertnanablessing@gmail.com>
2025-06-12 08:49:30 +0000

    snyk and sonaQ test

--- .github/workflows/test.py
+++
@@ -1,9 +0,0 @@
-name3: test.py
-import os
-
-# Get user input for a file to list
-filename = input("Enter the filename to list: ")
-
-# 🚨 Critical vulnerability: Command Injection!
-os.system(f"ls -l {filename}")
-
